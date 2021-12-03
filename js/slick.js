9fa6bf83870b6a3d10a6e7a6845d844862fdf183
Myakoo <yourflexium@protonmail.com>
2021-12-03 15:09:59 +0600

    Add js

---
+++ js/slick.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.slider-wrapper').slick({
+        dots: true,
+        infinite: false,
+        speed: 1000,
+        autoplay: false,
+        arrows: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                autoplay: false,
+            }
+        },
+	     {
+				breakpoint: 320
+		  }]
+    });
+});
